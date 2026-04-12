c2499a96cfeef4b000c97e57051b99ee05cb0766
luca6874 <lreinaga_24@alu.uabcs.mx>
2026-04-12 16:35:15 -0600

    eliminando archivos innecesarios ( .metadata)

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema Blockbuster iniciado");
+        System.out.println("Sistema Blockbuster, wa");
     }
 }
