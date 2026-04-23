858a7114ccbc0a6e5e00efbb75fdd46ba2875698
luca6874 <lreinaga_24@alu.uabcs.mx>
2026-04-23 14:41:56 -0600

    Actualización de la base de datos

--- database/database.sql
+++ database/database.sql
@@ -16,6 +16,72 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `primer_apellido` varchar(50) NOT NULL,
+  `segundo_apellido` varchar(50) DEFAULT NULL,
+  `correo_electronico` varchar(100) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `lvl_fidelidad` int DEFAULT '0',
+  PRIMARY KEY (`id_cliente`),
+  UNIQUE KEY `correo_electronico` (`correo_electronico`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `operaciones`
+--
+
+DROP TABLE IF EXISTS `operaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `operaciones` (
+  `id_operacion` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int DEFAULT NULL,
+  `id_videojuego` int DEFAULT NULL,
+  `id_usuario` int DEFAULT NULL,
+  `tipo` enum('RENTA','COMPRA') NOT NULL,
+  `monto` decimal(8,2) NOT NULL,
+  `descuento` decimal(5,2) DEFAULT '0.00',
+  `fecha_operacion` date NOT NULL,
+  `fecha_devolucion` date DEFAULT NULL,
+  PRIMARY KEY (`id_operacion`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_videojuego` (`id_videojuego`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `operaciones_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
+  CONSTRAINT `operaciones_ibfk_2` FOREIGN KEY (`id_videojuego`) REFERENCES `videojuegos` (`id_videojuego`),
+  CONSTRAINT `operaciones_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `operaciones`
+--
+
+LOCK TABLES `operaciones` WRITE;
+/*!40000 ALTER TABLE `operaciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `operaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuarios`
 --
 
@@ -23,14 +89,14 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `nombre` varchar(15) DEFAULT NULL,
-  `apellidos` varchar(30) DEFAULT NULL,
-  `fechaDeNacimiento` varchar(8) DEFAULT NULL,
-  `correo` varchar(70) DEFAULT NULL,
-  `telefono` varchar(10) DEFAULT NULL,
-  `rentasPrevias` varchar(4) DEFAULT NULL,
-  `nivelFidelidad` varchar(2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `correo` (`correo`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +105,40 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'asdfgj','waltuh@mail.com','1234'),(2,'luca','lucapro@mail.com','1234'),(3,'DArnel','atun@frijolmail.com','1234');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `videojuegos`
+--
+
+DROP TABLE IF EXISTS `videojuegos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `videojuegos` (
+  `id_videojuego` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `plataforma` varchar(20) DEFAULT NULL,
+  `genero` varchar(50) DEFAULT NULL,
+  `clasificacion` enum('E','T','M') DEFAULT NULL,
+  `anio_lanzamiento` year DEFAULT NULL,
+  `precio_renta` decimal(6,2) DEFAULT NULL,
+  `precio_compra` decimal(6,2) DEFAULT NULL,
+  `stock` int NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `puntos` int DEFAULT '0',
+  PRIMARY KEY (`id_videojuego`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `videojuegos`
+--
+
+LOCK TABLES `videojuegos` WRITE;
+/*!40000 ALTER TABLE `videojuegos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `videojuegos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +150,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-26  1:31:17
+-- Dump completed on 2026-04-23 14:39:43
